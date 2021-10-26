5ef61825a69246d645d7bd4eccf1256e2af2cb47
devesh-002 <75739508+devesh-002@users.noreply.github.com>
2021-10-26 16:33:59 +0530

    Update main.py

--- phase4/main.py
+++ phase4/main.py
@@ -12,48 +12,53 @@ while(True):
     print("Which table do you want to work on:-")
     print("1 for admin")
     print("2 for users")
-    print("3 for controls")
-    print("4 for tel_no_users")
-    print("5 for address_users")
-    print("6 for doctor")
-    print("7 for permission")
-    print("8 for patient")
-    print("9 for other_workers")
-    print("10 for grade_1")
-    print("11 for grade_2")
-    print("12 for department")
-    print("13 for maintenenceSchedule")
-    print("14 for medicine")
-    print("15 for treatment")
-    print("16 for medicalRecord")
-    print("17 for dosage")
-    print("18 to check cost for a medicine post discount")
-    print("19 to check age for a user")
-    print("20 to generate Report 1 which finds total medicine cost used by each patient along with patients names using JOIN technique")
-    print("21 to generate the second report")
-    print("22 to exit the user interface")
+    print("3 for tel_no_users")
+    print("4 for address_users")
+    print("5 for doctor")
+    print("6 for permission")
+    print("7 for patient")
+    print("8 for other_workers")
+    print("9 for medicine")
+    print("10 for treatment")
+    print("11 for medicalRecord")
+    print("12 to check cost for a medicine post discount")
+    print("13 to check age for a user")
+    print("14 to generate Report 1 which finds total medicine cost used by each patient along with patients names using JOIN technique")
+    print("15 to generate the second report which generates the patient details under a specific doctor")
+    print("16 to exit the user interface")
     command=int(input("Enter your preference:-"))
     print(command)
     if command==1:
         admin(con,cur)
     elif command==2:
         users(con,cur)
+ 
     elif command==3:
-        pass
-    elif command==4:
         tel_no_users(con,cur)
-    elif command==5:
+    elif command==4:
         address_users(con,cur)
-    elif command==6:
+    elif command==5:
         pass
-    elif command==7:
+    elif command==6:
         permission(con,cur)
-    elif command==18:
+    elif command==7:
+        patient(con,cur)
+    elif command==8:
+        other_workers(con,cur)
+    elif command==9:
+        medicine(con,cur)
+    elif command==10:
+        treatment(con,cur)
+    elif command==11: 
+        treatment(con,cur)
+    elif command==12:
         costForMedicine(con,cur)
-    elif command==19:
+    elif command==13:
         findAge(con,cur)
-    elif command ==20: # For Report 1 generation
+    elif command ==14: # For Report 1 generation
         report1(con,cur)
-    elif command==22:
+    elif command==15:
+        report2(con,cur)
+    elif command==16:
         print("Bye")
         exit()
